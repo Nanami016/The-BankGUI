879fbe555c1e462dc514278f34c401fb7adaf76d
Nanami <983921012@qq.com>
2019-12-12 15:53:10 +0800

    Create BankAccount.java

---
+++ BankAccount.java
@@ -0,0 +1,48 @@
+public class BankAccount
+{
+	private String id;
+	private double balance;
+	
+	public BankAccount()
+	{
+	}
+	
+	public BankAccount(String id)
+	{
+		this.id = id;
+	}
+	
+	public String getId()
+	{
+		return id;
+	}
+	
+	public double getBalance()
+	{
+		return balance;
+	}
+	
+	public void deposit(double amount)
+	{
+		balance += amount;
+	}
+	
+	public void withdraw(double amount)
+	{
+		balance -= amount;
+	}
+	
+	public void print()
+	{
+		System.out.println("卡号" + id + "余额" + balance);
+	}
+	
+	public static void main(String[] args)
+	{
+		BankAccount a = new BankAccount("ICBC001");
+		a.deposit(1000000);
+		System.out.println(a.getBalance());
+		a.deposit(100);
+		System.out.println(a.getBalance());
+	}
+}
